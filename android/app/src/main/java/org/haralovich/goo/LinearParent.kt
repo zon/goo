9db97e85353bc8a0d32637b5446bad55d2402c23
Matthew Z Haralovich <zon@haralovich.com>
2017-07-02 16:51:14 -0400

    Android vertical and horizontal layouts now support spacing.

--- android/app/src/main/java/org/haralovich/goo/LinearParent.kt
+++ android/app/src/main/java/org/haralovich/goo/LinearParent.kt
@@ -1,13 +1,15 @@
 package org.haralovich.goo
 
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.ScaleDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import com.fasterxml.jackson.databind.JsonNode
-
-enum class LinearDirection {
-    HORIZONTAL,
-    VERTICAL
-}
 
 class LinearParent(val direction: LinearDirection, val spacing: Float, padding: Inset) : SelfProps(padding) {
 
@@ -26,11 +28,33 @@ class LinearParent(val direction: LinearDirection, val spacing: Float, padding: 
     override fun update(view: View) {
         super.update(view)
         if (view is LinearLayout) {
+
             view.orientation = when (direction) {
                 LinearDirection.VERTICAL -> LinearLayout.VERTICAL
                 LinearDirection.HORIZONTAL -> LinearLayout.HORIZONTAL
             }
+
+            if (spacing != 0f) {
+                val size = (spacing * view.context.resources.displayMetrics.density).toInt()
+                view.showDividers = LinearLayout.SHOW_DIVIDER_MIDDLE
+                view.dividerDrawable = BitmapDrawable(
+                    view.context.resources,
+                    Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
+                )
+
+            } else {
+                view.showDividers = LinearLayout.SHOW_DIVIDER_NONE
+            }
+
+            Log.d("LIN", "SHOW " + view.showDividers)
+            Log.d("LIN", "DIV " + view.dividerDrawable + " " + view.dividerDrawable.minimumWidth)
+
         }
     }
 
+}
+
+enum class LinearDirection {
+    HORIZONTAL,
+    VERTICAL
 }
